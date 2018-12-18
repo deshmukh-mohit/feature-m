80b368bffc786553a747ef6200b27af96abead28
mohit_deshmukh <mohit.deshmukh@adapty.com>
2018-12-18 18:30:51 +0530

    DEC18

--- script_form.js
+++ script_form.js
@@ -11,8 +11,9 @@ document.getElementById('button').addEventListener('click',function(){
             gender: $("input[name=gender]:checked").val()
         }
         var params=JSON.stringify(person);
-       
-        console.log(params)
+        // var jsonData=JSON.parse('http://localhost:3000/person');
+
+        //console.log(params)
         $.ajax({
             url: 'http://localhost:3000/person',	
             type: 'post',
@@ -20,10 +21,27 @@ document.getElementById('button').addEventListener('click',function(){
             dataType: 'json',
             contentType: 'application/json',
             success: function () {
-               console.log(firstName);
+               
+               /* for(var i=0;i<jsonData.length;i++)
+                {
+                    var Html="<tr><td>"+jsonData[i].firstName+"</td><td>"+jsonData[i].lastName+"</td></tr>";
+                    $('#tbody').append(Html);
+                }*/
             } 
         });
  });
+// var jsonData=JSON.parse('http://localhost:3000/person');
+ var aj = $.ajax({
+        url: 'http://localhost:3000/person',
+        type: 'get',
+        dataType: 'json',
+    });
+ aj.done(function(data) {
+     for(var i=0;i<data.length;i++)
+           {
+             var Html="<tr><td>"+data[i].firstName+"</td><td>"+data[i].lastName+"</td></tr>";
+            $('#tbody').append(Html);
 
+           }
+ });
  
-    

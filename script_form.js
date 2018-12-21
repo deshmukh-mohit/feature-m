09ea059a741723c986acf018a7c1fb88cabb07a0
deshmukh-mohit <45788311+deshmukh-mohit@users.noreply.github.com>
2018-12-21 18:46:00 +0530

    dec20 work (#5)

--- script_form.js
+++ script_form.js
@@ -1,47 +1,82 @@
-document.getElementById('button').addEventListener('click',function(){
-	var person = {
-            firstName: $("#fn").val(),
-            lastName: $("#ln").val(),
-            emailId: $("#ID").val(),
-            password: $("#pwd").val(),
-            phone: $("#phn").val(),
-            birthday: $("#bday").val(),
-            address:$("#ad").val(),
-            country: $('#cn option:selected').text(),
-            gender: $("input[name=gender]:checked").val()
-        }
-        var params=JSON.stringify(person);
-        // var jsonData=JSON.parse('http://localhost:3000/person');
-
-        //console.log(params)
-        $.ajax({
-            url: 'http://localhost:3000/person',	
-            type: 'post',
-            data: params,
-            dataType: 'json',
-            contentType: 'application/json',
-            success: function () {
-               
-               /* for(var i=0;i<jsonData.length;i++)
-                {
-                    var Html="<tr><td>"+jsonData[i].firstName+"</td><td>"+jsonData[i].lastName+"</td></tr>";
-                    $('#tbody').append(Html);
-                }*/
-            } 
-        });
- });
-// var jsonData=JSON.parse('http://localhost:3000/person');
- var aj = $.ajax({
+$(document).ready(function(){
+    
+    var aj = $.ajax({
         url: 'http://localhost:3000/person',
         type: 'get',
         dataType: 'json',
     });
  aj.done(function(data) {
+
      for(var i=0;i<data.length;i++)
            {
-             var Html="<tr><td>"+data[i].firstName+"</td><td>"+data[i].lastName+"</td></tr>";
-            $('#tbody').append(Html);
+             var Html="<tr><td>"+data[i].firstName+"</td><td>"+data[i].lastName+"</td><td>"
+             +data[i].emailId+"</td><td>"+data[i].phone+"</td><td>"+data[i].gender+"</td><td>"
+             +data[i].birthday+"</td><td>"+data[i].country+"</td><td>"+data[i].address+"</td></tr>";
+             $('#tbody').append(Html);
 
            }
- });
- 
+    }); 
+
+    $( "#button" ).click(function(){
+       var person = {
+            firstName: $("#firstName").val(),
+            lastName: $("#lastName").val(),
+            emailId: $("#emailId").val(),
+            password: $("#password").val(),
+            phone: $("#phone").val(),
+            birthday: $("#birthday").val(),
+            address:$("#address").val(),
+            country: $('#country option:selected').text(),
+            gender: $("input[name=gender]:checked").val()
+        }
+        var params=JSON.stringify(person);
+        
+        var aj=$.ajax({
+            url: 'http://localhost:3000/person',    
+            type: 'post',
+            data: params,
+            dataType: 'json',
+            contentType: 'application/json',
+            success:  function (data) {
+                        var Html="<tr><td>"+data.firstName+"</td><td>"+data.lastName+"</td><td>"
+                        +data.emailId+"</td><td>"+data.phone+"</td><td>"+data.gender+"</td><td>"
+                        +data.birthday+"</td><td>"+data.country+"</td><td>"+data.address+"</td></tr>";
+                        $('#tbody').append(Html);
+                
+                 
+            }
+            
+           
+        });
+      });    
+/*      $(".input-box").focusout(function(){
+
+            var numItems = $('.input-box').length;
+        
+            if (numItems < 10){
+                alert("please dont keep feild empty \n");
+            }
+}) */
+ }); 
+    
+function validate(fieldId, msg, maxLen, minLen, regEx){   
+    var isValid = false;
+    $(fieldId).focusout(function(){
+        console.log("In");
+        var $this = $(this)
+        var fieldLen = $(this).val().length;
+        if(fieldLen < minLen || fieldLen > maxLen){
+            $this.next('.asteric').text(msg).show();
+            $this.addClass('input-box');
+        }else {
+            $this.next('.asteric').text(msg).hide();
+            $this.removeClass('input-box');
+        }
+        /*if(regEx) {
+            regEx.test($(this).val());
+        }*/
+    })
+}
+
+validate("#firstName","*",4,5,"/[^-\s]/");
+validate("#lastName","*",20,5,"/^\S*$/");
